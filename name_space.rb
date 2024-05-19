9525126759eddd3aaabf5b3b4ed2d97257ac74b4
Carlos Guilherme <guilhermeagripino@gmail.com>
2024-05-19 19:34:03 -0300

    Fix: usando name_space para evitar conflito com classe com mesmo nome e metodo com mesmo nome

--- name_space.rb
+++ name_space.rb
@@ -1,3 +1,5 @@
+=begin
+
 module ReverseWorld
     def self.puts text
         print text.reverse.to_s
@@ -6,3 +8,36 @@ end
 
 ReverseWorld::puts 'O resultado é'
 puts 'O resultado é'
+
+=end
+module Carioca
+    class Pessoa
+        def banda_preferida
+            "Capital Inicial"
+        end
+    end
+end
+
+module Baiano
+    class Pessoa
+        def banda_preferida
+            "Banda Eva"
+        end
+    end
+end
+
+module Pernambucano
+    class Pessoa
+        def banda_preferida
+            "Banda de Frevo"
+        end
+    end
+end
+
+p1 = Carioca::Pessoa.new
+p2 = Baiano::Pessoa.new
+p3 = Pernambucano::Pessoa.new
+
+puts "Sou Carioca e minha banda favorita é : " << p1.banda_preferida.to_s
+puts "Sou Baiano e minha banda favorita é : " << p2.banda_preferida.to_s
+puts "Sou Pernambucano e minha banda favorita é : " << p3.banda_preferida.to_s
