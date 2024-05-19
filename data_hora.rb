934f565c079e2e57b789ddc284b9e282fff5a757
Carlos Guilherme <guilhermeagripino@gmail.com>
2024-05-18 22:27:21 -0300

    convertendo string em time

--- data_hora.rb
+++ data_hora.rb
@@ -1,3 +1,6 @@
+require 'time'
+
+
 t = Time.new
 
 puts t.year.to_s << " - ano"
@@ -7,3 +10,15 @@ puts t.hour.to_s << "- hora"
 puts t. min.to_s << "- minuto"
 puts t.wday.to_s << "  - dia da semana"
 puts t.yday.to_s << " - dias que se passaram do ano"
+
+puts "###" * 10
+
+a = "16/08/1987 16:00:00"
+puts a
+puts a.class
+
+puts "###" * 10
+
+t1 = Time.parse(a)
+puts t1
+puts t1.class
